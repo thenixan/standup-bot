1a1b6cc3a711a087a5daf73fedf29733666b6fbb
Ilya Linnik <thenixan@gmail.com>
2019-09-04 19:24:18 +0300

    WIP

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+use std::env::VarError;
+use core::fmt;
+use std::fmt::Formatter;
+
+struct AppError(String);
+
+impl fmt::Debug for AppError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+type AppResult = std::result::Result<(), AppError>;
+
+impl From<VarError> for AppError {
+    fn from(_: VarError) -> Self {
+        AppError("Not all required parameters provided. See README file.".to_string())
+    }
+}
+
+fn main() -> AppResult {
+    dotenv().ok();
+
+    let api_key = env::var("API_KEY")?;
+
+    AppResult::Ok(())
 }
